a28f18a42d4db1e5a79e05497523cef3c811573e
Yvan J. Aquino <yvan.aquino@cloud-colosseum.net>
2022-01-15 22:06:07 +0000

    Updates to GoDFCX

--- godfcx/godfcx.go
+++ godfcx/godfcx.go
@@ -5,6 +5,31 @@ import (
 	"net/http"
 )
 
+// Interfaces
+type Message interface {
+	isMessage()
+}
+
+// Structs
+type WebhookRequest struct {
+	DetectIntentResponseID string          `json:"detectIntentResponseId,omitempty"`
+	IntentInfo             IntentInfo      `json:"intentInfo,omitempty"`
+	PageInfo               PageInfo        `json:"pageInfo,omitempty"`
+	SessionInfo            SessionInfo     `json:"sessionInfo,omitempty"`
+	FulfillmentInfo        FulfillmentInfo `json:"fulfillmentInfo,omitempty"`
+	Messages               []Messages      `json:"messages,omitempty"`
+	Text                   string          `json:"text,omitempty"`
+	LanguageCode           string          `json:"languageCode,omitempty"`
+}
+
+func (wr *WebhookRequest) FromRequest(r *http.Request) error {
+	err := json.NewDecoder(r.Body).Decode(wr)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 type IntentInfo struct {
 	LastMatchedIntent string  `json:"lastMatchedIntent,omitempty"`
 	DisplayName       string  `json:"displayName,omitempty"`
@@ -25,6 +50,12 @@ type FulfillmentInfo struct {
 	Tag string `json:"tag,omitempty"`
 }
 
+type Messages struct {
+	Text         Text   `json:"text,omitempty"`
+	ResponseType string `json:"responseType,omitempty"`
+	Source       string `json:"source,omitempty"`
+}
+
 type Text struct {
 	Text                      []string `json:"text,omitempty"`
 	RedactedText              []string `json:"redactedText,omitempty"`
@@ -33,11 +64,6 @@ type Text struct {
 
 func (t *Text) isMessage() {}
 
-type Source struct {
-	Text string `json:"text,omitempty"`
-	SSML string `json:"ssml,omitempty"`
-}
-
 type OutputAudioText struct {
 	AllowPlaybackInterruption bool `json:"allowPlaybackInterruption,omitempty"`
 	Source                    Source
@@ -45,39 +71,9 @@ type OutputAudioText struct {
 
 func (o *OutputAudioText) isMessage() {}
 
-type Messages struct {
-	Text         Text   `json:"text,omitempty"`
-	ResponseType string `json:"responseType,omitempty"`
-	Source       string `json:"source,omitempty"`
-}
-
-type WebhookRequest struct {
-	DetectIntentResponseID string          `json:"detectIntentResponseId,omitempty"`
-	IntentInfo             IntentInfo      `json:"intentInfo,omitempty"`
-	PageInfo               PageInfo        `json:"pageInfo,omitempty"`
-	SessionInfo            SessionInfo     `json:"sessionInfo,omitempty"`
-	FulfillmentInfo        FulfillmentInfo `json:"fulfillmentInfo,omitempty"`
-	Messages               []Messages      `json:"messages,omitempty"`
-	Text                   string          `json:"text,omitempty"`
-	LanguageCode           string          `json:"languageCode,omitempty"`
-}
-
-func (wr *WebhookRequest) FromRequest(r *http.Request) error {
-	err := json.NewDecoder(r.Body).Decode(wr)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-type Message interface {
-	isMessage()
-}
-
-type FulfillmentResponse struct {
-	// https://pkg.go.dev/google.golang.org/genproto/googleapis/cloud/dialogflow/cx/v3beta1#ResponseMessage
-	Messages      []Message
-	MergeBehavior int
+type Source struct {
+	Text string `json:"text,omitempty"`
+	SSML string `json:"ssml,omitempty"`
 }
 
 type WebhookResponse struct {
@@ -87,34 +83,36 @@ type WebhookResponse struct {
 	Payload             map[string]string   `json:"payload,omitempty"`
 }
 
+type FulfillmentResponse struct {
+	// https://pkg.go.dev/google.golang.org/genproto/googleapis/cloud/dialogflow/cx/v3beta1#ResponseMessage
+	Messages      []Message
+	MergeBehavior int
+}
+
 func (wr *WebhookResponse) TextResponse(w http.ResponseWriter, msgs ...string) {
-	t := Text{
+	m := Text{
 		Text:                      msgs,
 		AllowPlaybackInterruption: true,
 	}
 
-	r := WebhookResponse{
-		FulfillmentResponse: FulfillmentResponse{
-			Messages:      []Message{&t},
-			MergeBehavior: 0,
-		},
+	wr.FulfillmentResponse = FulfillmentResponse{
+		Messages:      []Message{&m},
+		MergeBehavior: 0,
 	}
-	json.NewEncoder(w).Encode(r)
+	json.NewEncoder(w).Encode(wr)
 }
 
 func (wr *WebhookResponse) SSMLResponse(w http.ResponseWriter, msg string) {
-	t := OutputAudioText{
+	m := OutputAudioText{
 		AllowPlaybackInterruption: true,
 		Source: Source{
 			SSML: msg,
 		},
 	}
 
-	r := WebhookResponse{
-		FulfillmentResponse: FulfillmentResponse{
-			Messages:      []Message{&t},
-			MergeBehavior: 0,
-		},
+	wr.FulfillmentResponse = FulfillmentResponse{
+		Messages:      []Message{&m},
+		MergeBehavior: 0,
 	}
-	json.NewEncoder(w).Encode(r)
+	json.NewEncoder(w).Encode(wr)
 }
